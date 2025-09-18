da88d912585bf88f7af3d52ea2a93add9b1908f2
bmmaier <bmm8699@rit.edu>
2025-09-18 12:23:50 -0400

    added sr. dev webpage

---
+++ srDevPortal/assets/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $title?></title>
+        <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    </head>
+    <body>
+        <header>
+            <img id="logo" src="assets/media/RIT_rgb_hor_k.svg" alt="Rochester Institute of Technology">
+        </header>
---
+++ srDevPortal/index.php
@@ -0,0 +1,31 @@
+<?php
+    $page = "";
+    $group = "home";
+    $path = "";
+    $title = "Senior Development";
+    include ($path . "assets/inc/header.php");
+?>
+
+        <section class="title-container">
+            <h1 id="title">Senior Development</h1>
+            <h3 id="subtitle">Self-Assessment</h3>
+        </section>
+        <section class="question-container">
+            <div class="q-box">
+                <p class="q-section-title">
+                    Personal Information
+                </p>
+            </div>
+            <div class="q-box">
+                <p class="q-section-title">
+                    Personality Assessment
+                </p>
+            </div>
+            <div class="q-box">
+                <p class="q-section-title">
+                    Technical Assessment
+                </p>
+            </div>
+        </section>
+    </body>
+</html>
