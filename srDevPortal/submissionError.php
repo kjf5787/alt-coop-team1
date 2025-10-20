8724b29980f50cc75a6de42bb99e780c268a241e
vjp8609 <vjp8609@g.rit.edu>
2025-10-20 00:18:59 -0400

    Update submissionError.php

--- srDevPortal/submissionError.php
+++ srDevPortal/submissionError.php
@@ -1,3 +1,16 @@
 <?php
-
-echo("You already submitted this form.");
+    $page = "";
+    $group = "home";
+    $path = "";
+    $title = "error alert";
+    require_once ($path . "assets/inc/header.php");
+?>
+<section class="error-container">
+    <div class="e-box">
+        <div class="error-box">
+            <p>ERROR:You already submitted this form</p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
